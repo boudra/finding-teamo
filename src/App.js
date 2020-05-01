65a8ca5016de77fc5dd55cffa26f588e43f29c77
Mohamed Boudra <boudra.moha@gmail.com>
2020-05-01 09:49:56 +0200

    feat: add project list comp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import "./App.scss";
+
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./App.scss";
+import ProjectList from "./ProjectList.js";
+
+const project = {
+  id : 1,
+  title : "Something something",
+  description :
+      "Amet quis velit repellendus ullam tempora quidem. Cumque asperiores reprehenderit eaque ducimus animi Nesciunt quod delectus eaque delectus quam voluptates Iure earum iusto harum voluptatem ex placeat? Provident velit labore.",
+  skillsNeeded : [ "Copywriter" ],
+  owner : {
+    id : 1,
+    fullName : "John Doe",
+    skills : [
+      "Software Development",
+      "ReactJS",
+      "Redux",
+    ]
+  },
+  members : [
+    {id : 2, fullName : "Jane Doe", skills : [ "Designer", "Adobe Photoshop" ]}
+  ]
+};
+
+const initialState = {
+  projects : (new Array(10).fill(project))
+};
 
 export default function App() {
-  return <h1>Finding Teamo</h1>;
+  return <div><h1>Finding
+      Teamo</h1>
+    <div>
+      <ProjectList projects={initialState.projects}/>
+      </div>
+  </div>;
 }
