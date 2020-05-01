4508032924a7aa0dc512f541bd4e0eb875415e51
Mohamed Boudra <boudra.moha@gmail.com>
2020-05-01 13:08:42 +0200

    feat: move reducers into src

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import reducers from "../reducers";
+import reducers from "./reducers";
 
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
