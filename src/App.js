2528ef64baebdd1192367e1051e7e63f079b44f5
Mohamed Boudra <boudra.moha@gmail.com>
2020-05-01 10:00:10 +0200

    style: format

--- src/App.js
+++ src/App.js
@@ -6,34 +6,32 @@ import ReactDOM from "react-dom";
 import ProjectList from "./ProjectList.js";
 
 const project = {
-  id : 1,
-  title : "Something something",
-  description :
-      "Amet quis velit repellendus ullam tempora quidem. Cumque asperiores reprehenderit eaque ducimus animi Nesciunt quod delectus eaque delectus quam voluptates Iure earum iusto harum voluptatem ex placeat? Provident velit labore.",
-  skillsNeeded : [ "Copywriter" ],
-  owner : {
-    id : 1,
-    fullName : "John Doe",
-    skills : [
-      "Software Development",
-      "ReactJS",
-      "Redux",
-    ]
+  id: 1,
+  title: "Something something",
+  description:
+    "Amet quis velit repellendus ullam tempora quidem. Cumque asperiores reprehenderit eaque ducimus animi Nesciunt quod delectus eaque delectus quam voluptates Iure earum iusto harum voluptatem ex placeat? Provident velit labore.",
+  skillsNeeded: ["Copywriter"],
+  owner: {
+    id: 1,
+    fullName: "John Doe",
+    skills: ["Software Development", "ReactJS", "Redux"],
   },
-  members : [
-    {id : 2, fullName : "Jane Doe", skills : [ "Designer", "Adobe Photoshop" ]}
-  ]
+  members: [
+    { id: 2, fullName: "Jane Doe", skills: ["Designer", "Adobe Photoshop"] },
+  ],
 };
 
 const initialState = {
-  projects : (new Array(10).fill(project))
+  projects: new Array(10).fill(project),
 };
 
 export default function App() {
-  return <div><h1>Finding
-      Teamo</h1>
+  return (
     <div>
-      <ProjectList projects={initialState.projects}/>
+      <h1>Finding Teamo</h1>
+      <div>
+        <ProjectList projects={initialState.projects} />
       </div>
-  </div>;
+    </div>
+  );
 }
--- src/ProjectList.js
+++ src/ProjectList.js
@@ -4,16 +4,18 @@ import ReactDOM from "react-dom";
 import "./ProjectList.scss";
 
 function ProjectItem(project) {
-  return <div>
-    <h2>{project.title}</h2>
-    <p>{project.description}</p>
-  </div>;
+  return (
+    <div>
+      <h2>{project.title}</h2>
+      <p>{project.description}</p>
+    </div>
+  );
 }
 
-export default function({projects}) {
-  return <section className="project-list">
-    <div>
-      {projects.map(ProjectItem)}
-    </div>
-  </section>;
+export default function ({ projects }) {
+  return (
+    <section className="project-list">
+      <div>{projects.map(ProjectItem)}</div>
+    </section>
+  );
 }
